f43270693462b1b4a8c40871258bea1106f0fb79
Jason <katsuutan0408@gmail.com>
2024-11-25 14:24:29 -0800

    Add display and clear for weather information and error message when location is invalid

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import "./styles.css";
 import searchButton from "./search";
 
-export { getWeather };
+// eslint-disable-next-line no-use-before-define
+export { clearDisplayData, getWeather };
 
 // Processes the WeatherData and returns an object with only data used on the site.
 function convertData(weatherData) {
@@ -16,7 +17,28 @@ function convertData(weatherData) {
 
 // Takes in the converted Weather Data object and manipulates DOM to display information.
 function displayData(convertedData){
+    const weatherDiv = document.getElementById('weather-container');
 
+    const location = document.createElement('div');
+    location.textContent = convertedData.location;
+
+    const tempF = document.createElement('div');
+    tempF.textContent = `${convertedData.currentTempF}\u00B0F`;
+
+    const tempC = document.createElement('div');
+    tempC.textContent = `${convertedData.currentTempC}\u00B0C`;
+
+
+    weatherDiv.appendChild(location);
+    weatherDiv.appendChild(tempF);
+    weatherDiv.appendChild(tempC);
+}
+
+function clearDisplayData(){
+    const weatherDiv = document.getElementById('weather-container');
+    while (weatherDiv.firstChild) {
+        weatherDiv.removeChild(weatherDiv.firstChild);
+    }
 }
 
 // Grabs Weather Data using API.
@@ -24,16 +46,17 @@ async function getWeather(location) {
     try {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=7TVV8XALU8HFN2D4GRPBMWCB5`, {mode: 'cors'});
         const weatherData = await response.json();
-        console.log(weatherData); // Testing only, remove later
 
+        // Converts raw data and turns into relevant object for us.
         const convertedData = convertData(weatherData);
-        console.log(convertedData) // Testing only, remove later
 
         // Calls a function to Display convertedData to page utilizing DOM manipulation
-        // displayData(convertedData);
+        displayData(convertedData);
 
     } catch (error) {
         console.error(error);
+        const errorDiv = document.getElementById('weather-container');
+        errorDiv.textContent = 'Error 404: ' + error;
     }
 }
 
--- src/search.js
+++ src/search.js
@@ -1,9 +1,11 @@
-import { getWeather } from ".";
+import { clearDisplayData, getWeather } from ".";
 
 export default function searchButton() {
     const searchBtn = document.getElementById('search-btn');
     searchBtn.addEventListener('click', (e) => {
         e.preventDefault();
+
+        clearDisplayData(); // Refreshes display upon search button press.
 
         const location = document.getElementById('location').value;
 
